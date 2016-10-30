21f12a8288000278ed890b558891ad3bf67c672e
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-10-30 16:03:42 +0100

    Fix

--- src/level.cpp
+++ src/level.cpp
@@ -1,9 +1,9 @@
+#include <algorithm>
+#include <glm/gtc/noise.hpp>
+#include <glm/gtx/io.hpp>
 #include <level.hpp>
 #include <mos/util.hpp>
-#include <glm/gtx/io.hpp>
-#include <glm/gtc/noise.hpp>
 #include <queue>
-#include <algorithm>
 
 Level::Level(mos::Assets &assets, const glm::vec2 &resolution)
     : time_(0.0f), times_(0), camera_(resolution) {
@@ -17,25 +17,17 @@ Level::Level(mos::Assets &assets, const glm::vec2 &resolution)
 
 Level::~Level() {}
 
-void Level::camera_left(const bool left) {
-  camera_.left = left;
-}
+void Level::camera_left(const bool left) { camera_.left = left; }
 
-void Level::camera_right(const bool right) {
-  camera_.right = right;
-}
+void Level::camera_right(const bool right) { camera_.right = right; }
 
-void Level::camera_forward(const bool forward) {
-  camera_.forward = forward;
-}
+void Level::camera_forward(const bool forward) { camera_.forward = forward; }
 
 void Level::camera_backward(const bool backward) {
   camera_.backward = backward;
 }
 
-void Level::camera_zoom_in(const bool zoom_in) {
-  camera_.zoom_in = zoom_in;
-}
+void Level::camera_zoom_in(const bool zoom_in) { camera_.zoom_in = zoom_in; }
 
 void Level::camera_zoom_out(const bool zoom_out) {
   camera_.zoom_out = zoom_out;
@@ -45,25 +37,30 @@ void Level::update(const float dt) {
   time_ += dt;
   Entities new_entities;
 
-  //if (time_ > 3.0f) {
-  if (times_ < 10){
+  // if (time_ > 3.0f) {
+  if (times_ < 10) {
     times_++;
-    for (auto & e : entities_) {
-      for (auto &d : e->exits){
+    for (auto &e : entities_) {
+      for (auto &d : e->exits) {
         if (!d.next) {
           std::shared_ptr<Entity> next;
           // auto v = glm::simplex(e->model().position());
 
-          //if (v < 0.0f) {
-            next = std::make_shared<Corridor>(d.transform, corridor_);
+          // if (v < 0.0f) {
+          next = std::make_shared<Corridor>(d.transform, corridor_);
           //}
-            /*
-          else {
-            next = std::make_shared<Stairs>(d.transform, stairs_);
-          }*/
-          if (std::none_of(entities_.begin(), entities_.end(), [&](const Entity::SharedEntity& e0){
-                           return e0->intersects(*next);
-        })) {
+          /*
+        else {
+          next = std::make_shared<Stairs>(d.transform, stairs_);
+        }*/
+          if (std::none_of(entities_.begin(), entities_.end(),
+                           [&](const Entity::SharedEntity &e0) {
+                             return e0->intersects(*next);
+                           }) &&
+              std::none_of(new_entities.begin(), new_entities.end(),
+                           [&](const Entity::SharedEntity &e0) {
+                             return e0->intersects(*next);
+                           })) {
             d.next = next;
             new_entities.push_back(d.next);
           }
@@ -94,6 +91,4 @@ Level::Boxes Level::boxes() {
   return boxes;
 }
 
-mos::Camera Level::camera() const {
-   return camera_.camera();
-}
+mos::Camera Level::camera() const { return camera_.camera(); }
