76ebd62f54d32753b1419e39c709b1b0658c9b61
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-01 17:08:51 +0100

    Check

--- src/stairs_down.cpp
+++ src/stairs_down.cpp
@@ -3,8 +3,7 @@
 
 StairsDown::StairsDown(const glm::mat4 &transform,
                const mos::Model model,
-const unsigned int max_length)
-    : model_(model) {
+const unsigned int max_length) {
   model_.transform = transform;
   length_ = uint((glm::abs(glm::simplex(model_.position()))) * max_length + 1.0f);
 
@@ -12,13 +11,13 @@ const unsigned int max_length)
       Door(transform *
           glm::translate(glm::mat4(1.0f), glm::vec3(length_, 0.0f, -length_))));
 
-  auto vertices = model_.mesh->vertices();
-  for (auto &v : vertices) {
-    v.position = glm::vec3(transform * glm::vec4(v.position, 1.0f));
+  for (int i = 0; i < length_; i++) {
+    auto m = model;
+    m.transform = glm::translate(glm::mat4(1.0f), glm::vec3(i, 0.0f, -i));
+    model_.models.push_back(m);
   }
-
   box_ = mos::Box::create_from_model(model_);
-  box_.extent -= 0.01f;
+  box_.extent -= 0.001;
 }
 
 mos::Model StairsDown::model() { return model_; }
