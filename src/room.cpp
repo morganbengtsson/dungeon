f0bc3bc098404eee9f0adbfe27fa3137a28dc6b6
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-02 12:28:56 +0100

    Round room

--- src/room.cpp
+++ src/room.cpp
@@ -7,13 +7,7 @@ Room::Room(const glm::mat4 &transform, const mos::Model &model) : model_(model) 
   size_.y = uint(glm::abs(glm::simplex(model_.position()) * 10.0f + 5.0f)) + 2.0f;
   model_.transform *= glm::scale(glm::mat4(1.0f), glm::vec3(size_.x, size_.y, 1.0f));
 
-  std::vector<mos::Vertex> all_vertices;
-  for (auto &v0 : model.mesh->vertices()) {
-    auto vt0 = v0;
-    vt0.position = glm::vec3(model_.transform * glm::vec4(v0.position, 1.0f));
-    all_vertices.push_back(vt0);
-  }
-  box_ = mos::Box(all_vertices.begin(), all_vertices.end(), glm::mat4(1.0f));
+  box_ = mos::Box::create_from_model(model_);
   box_.extent -= 0.01;
 
   exits.push_back(
--- src/round_room.cpp
+++ src/round_room.cpp
@@ -1,21 +1,15 @@
-#include <room.hpp>
+#include <round_room.hpp>
 #include <glm/gtc/noise.hpp>
 
 RoundRoom::RoundRoom(const glm::mat4 &transform, const mos::Model &model) : model_(model) {
   model_.transform = transform;
-  size_.x = uint(glm::abs(glm::simplex(model_.position()) * 10.0f + 5.0f)) + 2.0f;
-  size_.y = uint(glm::abs(glm::simplex(model_.position()) * 10.0f + 5.0f)) + 2.0f;
-  model_.transform *= glm::scale(glm::mat4(1.0f), glm::vec3(size_.x, size_.y, 1.0f));
+  diameter_ = uint(glm::abs(glm::simplex(model_.position()) * 10.0f + 5.0f)) + 2.0f;
+  model_.transform *= glm::scale(glm::mat4(1.0f), glm::vec3(diameter_, diameter_, 1.0f));
 
-  std::vector<mos::Vertex> all_vertices;
-  for (auto &v0 : model.mesh->vertices()) {
-    auto vt0 = v0;
-    vt0.position = glm::vec3(model_.transform * glm::vec4(v0.position, 1.0f));
-    all_vertices.push_back(vt0);
-  }
-  box_ = mos::Box(all_vertices.begin(), all_vertices.end(), glm::mat4(1.0f));
+  box_ = mos::Box::create_from_model(model_);
   box_.extent -= 0.01;
 
+  /*
   exits.push_back(
       Door(transform *
           glm::translate(glm::mat4(1.0f), glm::vec3(size_.x, 0.0f, 0.0f))));
@@ -34,7 +28,7 @@ RoundRoom::RoundRoom(const glm::mat4 &transform, const mos::Model &model) : mode
       Door(transform *
           glm::translate(glm::mat4(1.0f), glm::vec3(size_.x / 2.0f, size_.y / 2.0f, 0.0f)) *
           glm::rotate(glm::mat4(1.0f), glm::half_pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f))));
-
+  */
 }
 
 mos::Model RoundRoom::model() {
