7bf1c68fa1ef14ba74169e026b4e85d5030dd289
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-06 18:03:11 +0100

    Meshes and stuff.

--- src/room.cpp
+++ src/room.cpp
@@ -12,16 +12,37 @@ Room::Room(mos::Assets &assets, const glm::mat4 &transform) {
 
   const auto entry_pos = mos::position(transform);
 
-  auto floor_model = assets.model("room_floor.model");
+  auto room_type = simplex_index(entry_pos, 3);
 
-  std::vector<mos::Model> edge_models{assets.model("room_edge1.model"),
-                                      assets.model("room_edge1.model"),
-                                      assets.model("room_edge2.model"),
-                                      assets.model("room_edge3.model"),
-                                      assets.model("room_edge4.model")};
+  //TODO: Move to own class.
+  std::vector<mos::Model> floor_models;
+  std::vector<mos::Model> edge_models;
+  mos::Model corner_model;
+  mos::Model entry_model;
+  if (room_type == 0) {
+    corner_model = assets.model("room_corner.model");
+    entry_model = assets.model("room_entry.model");
+    floor_models = {assets.model("room_floor.model")};
+    edge_models = {assets.model("room_edge1.model"),
+                   assets.model("room_edge1.model"),
+                   assets.model("room_edge2.model"),
+                   assets.model("room_edge3.model"),
+                   assets.model("room_edge4.model")};
+  }
+  else if (room_type == 1) {
+    corner_model = assets.model("room_corner_metal.model");
+    entry_model = assets.model("room_entry_metal.model");
+    floor_models = {assets.model("room_floor_metal.model")};
+    edge_models = {assets.model("room_edge_metal0.model")};
+  }
+  else if (room_type == 2) {
+    corner_model = assets.model("room_corner_wood.model");
+    entry_model = assets.model("room_entry_wood.model");
+    floor_models = {assets.model("room_floor_wood.model")};
+    edge_models = {assets.model("room_edge_wood0.model")};
+  }
 
-  auto corner_model = assets.model("room_corner.model");
-  auto entry_model = assets.model("room_entry.model");
+
 
   //Entry door
   entry_model.transform = glm::rotate(glm::translate(glm::mat4(1.0f), {.5f, .0f, .0f}), 0.0f, {.0f, .0f, 1.f});
@@ -50,6 +71,7 @@ Room::Room(mos::Assets &assets, const glm::mat4 &transform) {
                                 {.0f, .0f, 1.f});
 
     auto & edge_model = edge_models[simplex_index(mos::position(t1),edge_models.size())];
+    auto & floor_model = floor_models[0];
     if (x != float(size_.x / 2)) {
       edge_model.transform = t0;
       room_.models.push_back(edge_model);
@@ -58,6 +80,7 @@ Room::Room(mos::Assets &assets, const glm::mat4 &transform) {
       edge_model.transform = t1;
       room_.models.push_back(edge_model);
     } else {
+
       floor_model.transform = t0;
       room_.models.push_back(floor_model);
 
