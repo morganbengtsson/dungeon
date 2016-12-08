e90be35b11e756a6b369c92b0d2f909eef49bb9a
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-08 13:07:00 +0100

    Checkpoint.

--- src/door.cpp
+++ src/door.cpp
@@ -1,6 +1,7 @@
 #include <door.hpp>
 
 Door::Door(mos::Assets &assets, const glm::mat4 &transform) : transform(transform), model_(assets.model("door.model")) {
+  model_.transform = transform;
 }
 
 mos::Model Door::model() const {
