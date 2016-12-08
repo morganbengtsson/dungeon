95c9fdab13b3cf3a6bba8196a40023ffb97dd5e2
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2016-12-08 17:48:41 +0100

    Fix corridor

--- src/entities/corridor.cpp
+++ src/entities/corridor.cpp
@@ -19,20 +19,6 @@ Corridor::Corridor(mos::Assets &assets, const glm::mat4 &transform)
       Door(assets, transform *
            glm::translate(glm::mat4(1.0f), glm::vec3(length_, 0.0f, 0.0f))));
 
-  unsigned int right = uint((glm::abs(glm::simplex(model_.position())) * length_));
-  exits.push_back(
-      Door(assets, transform *
-           glm::translate(glm::mat4(1.0f), glm::vec3(0.5f + right, -0.5f, 0.0f)) *
-           glm::rotate(glm::mat4(1.0f), -glm::half_pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f))));
-
-  unsigned int left = uint((glm::abs(glm::simplex(model_.position())) * length_));
-  exits.push_back(
-      Door(assets, transform *
-           glm::translate(glm::mat4(1.0f), glm::vec3(0.5f + left, 0.5f, 0.0f)) *
-           glm::rotate(glm::mat4(1.0f), glm::half_pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f))));
-
-  //model_.overlay(glm::linearRand(glm::vec4(0.0f), glm::vec4(1.0f)));
-
   box_ = mos::Box::create_from_model(model_);
   box_.extent -= 0.001;
 }
